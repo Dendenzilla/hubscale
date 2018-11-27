8bb4dca5c5c48e2a4894406c997bde47366182d8
Martin Adriaensen <martinadriaensen@gmail.com>
2018-11-27 15:58:05 +0100

    update homepage

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slide').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 2000,
+    });
+  });
